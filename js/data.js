ef570df34f75ffe44674989fec669d8e510a57ed
Pr0x1mas <50422789+Pr0x1mas@users.noreply.github.com>
2021-10-20 19:20:48 +0100

    tracking finished

--- js/data.js
+++ js/data.js
@@ -1,10 +1,29 @@
 var dataTableContainer = document.getElementById("dataTableContainer");
+var frameDelta;
 
 var fields = [
     ["Frame", "coordIndex"],
     ["X", "mass.realData[coordIndex].x"],
-    ["Y", "mass.realData[coordIndex].y"]
-]
+    ["Y", "mass.realData[coordIndex].y"],
+    ["V", "getVelocity(massIndex,coordIndex)"]
+];
+
+function getVelocity(inputMassIndex, inputCoordIndex){
+    if(inputCoordIndex in masses[inputMassIndex].realData && inputCoordIndex - 1 in masses[inputMassIndex].realData){
+
+        s = distanceBetweenPoints(masses[inputMassIndex].realData[inputCoordIndex], masses[inputMassIndex].realData[inputCoordIndex - 1]);
+        t = frameDelta;
+
+        console.log(s);
+        console.log(t);
+
+        return s/t
+    } else {
+        console.log(inputCoordIndex);
+        console.log(inputMassIndex);
+        return "Nah man";
+    }
+}
 
 function distanceBetweenPoints(point1, point2){
     var a = Math.abs(point1.x - point2.x);
@@ -13,16 +32,16 @@ function distanceBetweenPoints(point1, point2){
     var c = Math.sqrt(Math.pow(a, 2) + Math.pow(a, 2))
 
     return c
-}
+};
 
 function addData(){
+    frameDelta = 1 / mainVideo.framerate;
+
     dataTableContainer.innerHTML = "";
     masses.forEach((mass, massIndex) => {
 
         title = document.createElement("h2");
         title.innerText = mass.name;
-
-        title.style.color = massColours[massIndex][0];
 
         table = document.createElement("table");
 
@@ -38,6 +57,7 @@ function addData(){
         table.appendChild(tableHeader);
 
         mass.realData.forEach((coord, coordIndex) => {
+            console.log(coord);
             row = document.createElement("tr");
 
             fields.forEach((field) => {
